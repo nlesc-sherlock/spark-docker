9bdf7954d3ab26ecda53aa70a02d7e25286e3f77
Romulo Goncalves <r.goncalves@esciencecenter.nl>
2015-12-01 17:56:37 +0100

    How to run a docker container for caffee in Spark

---
+++ caffe/README.md
@@ -0,0 +1,12 @@
+# Compile
+
+```
+sbt package
+```
+
+# run
+
+```
+$SPARK_HOME/bin/spark-submit --class caffeApp --master <sparkURL> target/scala-2.10/caffe-app_2.10-1.0.jar
+```
+
---
+++ caffe/src/main/scala/ListDirApp.scala
@@ -0,0 +1,48 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.fs.Path
+import org.apache.spark.rdd.RDD
+import org.apache.spark.input.PortableDataStream
+import scala.sys.process._
+import java.io.File.createTempFile
+import java.io.FileOutputStream
+import java.nio.file.Path
+import java.nio.file.Files.createTempDirectory
+import java.nio.file.Files.createFile
+
+
+object ListDirApp {
+
+	def mapDocker(binaryFile :(String, PortableDataStream)):(String,String) = {
+		print("The filename is: " + binaryFile._1 + " !!!\n")
+		val tmpDir = createTempDirectory("caffe")
+		val f = createFile(tmpDir.resolve("caffe.img"))	
+		val fOut = new FileOutputStream(f.toFile())	
+		fOut.write(binaryFile._2.toArray())	
+		fOut.close()
+		
+		//Call Docker container
+		// docker run -v /home/romulo/sherlock/tmp_dir:/data caffe data/f.basename
+		val cmd ="docker run --rm -v " + tmpDir.toString() + ":/data caffe data/caffe.img" 
+		val output = cmd.!!
+
+		//f.deleteOnExit()
+		return (binaryFile._1, output)
+	}
+
+	def main(args: Array[String]) {
+		val conf = new SparkConf().setAppName("List Dir App")
+		val sc = new SparkContext(conf)
+		val hconf = sc.hadoopConfiguration
+
+		val binF = sc.binaryFiles("/home/romulo/sherlock/in_dir")
+		print("The list of ifiles is: ")
+		val fileLengths = binF.map(T => this.mapDocker(T))
+		//binF.foreach{ T => mapDocket(T._1,T._2) } 
+		fileLengths.foreach{println}
+		print("done!\n")
+	}
+}
